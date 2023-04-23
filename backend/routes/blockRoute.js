4f4c7180919f3942a267e01561627d756ac0c783
Pavel Emelianov <emeliarulit@gmail.com>
2023-04-23 22:02:38 +0300

    Initial commit

---
+++ backend/routes/blockRoute.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+const { getBlocks } = require('../controllers/blockController');
+
+router.get('/blocks', getBlocks);
+
+module.exports = router;
---
+++ backend/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv').config();
+
+const PORT = process.env.PORT || 8000;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.status(200).send('Hello');
+});
+
+app.use('/api', require('./routes/loginRoute'));
+app.use('/api', require('./routes/blockRoute'));
+
+app.listen(PORT, () => console.log('server ...'));
